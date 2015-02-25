6f5a23c52be5beebedabd240b5551453b56b5194
Chris Ovenden <cjo20@gs011461.local>
2015-02-25 17:03:07 +0000

    person models

--- node/models.js
+++ node/models.js
@@ -1,14 +1,33 @@
 var Sequelize = require('sequelize'),
     sequelize = require('./db').sequelize;
 
-var models = {
-    User: sequelize.define('User', {
-        username: Sequelize.STRING,
-        password: Sequelize.STRING
-    })
+var Nationality= sequelize.define('Nationality', {
+    nationality: Sequelize.STRING
+});
+
+var Centre = sequelize.define('Centre', {
+    name: Sequelize.STRING,
+    current_beds_male: Sequelize.INTEGER,
+    current_beds_female: Sequelize.INTEGER,
+    current_beds_ooc: Sequelize.INTEGER
+});
+
+var Person = sequelize.define('Person', {
+        cid_id: Sequelize.STRING,
+        gender: Sequelize.STRING(1)
+    });
+Person.hasOne(Nationality);
+Person.hasOne(Centre, {
+    as: 'current_location'
+});
+
+
+
+/*sequelize
+    .sync({ force: true });*/
+
+exports.models = {
+    Person: Person,
+    Nationality: Nationality,
+    Centre: Centre
 };
-/*
-sequelize
-    .sync({ force: true });
-*/
-exports.models = models;
