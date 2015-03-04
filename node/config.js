715b593ee22f6c75340edc3704ed43cc5d5c9b05
Chris Ovenden <cjo20@gs011461.local>
2015-03-04 15:38:28 +0000

    fix CONFIG.project_path

--- node/config.js
+++ node/config.js
@@ -4,23 +4,29 @@ var map= {
         'DB_HOST' : 'host',
         'DB_NAME' : 'database',
     },
-    get_from_env = Object.keys(map);
-var cur_env       = process.env.NODE_ENV || "development";
-var config = {};
-for(var v in get_from_env) {
-    var key = get_from_env[v];
-    if(process.env[key]){
-        console.log("setting " + key + " to " + process.env[key]);
-        config[map[key]] = process.env[key];
-    } else {
-        console.log("No environment variable " + key + ". Using config.json.");
-        config    = require('../config/config.json')[cur_env];
-        break;
+    get_from_env = Object.keys(map),
+    directory_path_components = process.cwd().split('/'),
+    project_path = directory_path_components.slice(0, directory_path_components.length -1).join('/'),
+    cur_env = process.env.NODE_ENV || "development",
+    config = {},
+    v;
+
+for(v in get_from_env) {
+    if (get_from_env.hasOwnProperty(v)) {
+        var key = get_from_env[v];
+        if (process.env[key]) {
+            console.log("setting " + key + " to " + process.env[key]);
+            config[map[key]] = process.env[key];
+        } else {
+            console.log("No environment variable " + key + ". Using config.json.");
+            config = require('../config/config.json')[cur_env];
+            break;
+        }
     }
 }
 
 CONFIG = {
-    project_path: process.cwd(),
+    project_path: project_path,
     db: {
         name: config.database,
         user: config.username,
