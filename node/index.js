ac17410170aee886762c4bf08707ac4d9e30ba75
Chris Ovenden <cjo20@gs011461.local>
2015-03-13 16:20:10 +0000

    SASS essential files from CTS and outputting

--- node/index.js
+++ node/index.js
@@ -1,2 +1,39 @@
-var server = require("./server");
+var sass  = require('node-sass'),
+    fs = require('fs');
+    server = require("./server"),
+    CONFIG = require("./config").config,
+
+    cssOutputPath = CONFIG.project_path + '/public/css/main.css',
+    sassBase = CONFIG.project_path + '/sass/base.scss';
+    console.log('sassBase: ' + sassBase);
+
+sass.render(
+    {
+        file: sassBase,
+        //sourceMap: true,
+        //outFile: cssOutputPath
+    },
+    function(err, result) {
+        console.log(err ? 'sass error: ' + err : 'sass rendered! ');
+        console.log(result);
+
+        /*var output = fs.open(cssOutputPath, 'w', function() {
+            output.write(result.css);
+            output.close();
+        });*/
+
+        var stream = fs.createWriteStream(cssOutputPath, {
+            flags: 'w',
+            encoding: null,
+            fd: null,
+            mode: 0666
+        });
+        stream.write(result.css);
+        stream.end();
+
+
+    }
+);
+
+
 server.start();
