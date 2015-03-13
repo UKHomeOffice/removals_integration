53357c5791b35cfa5dae87c0782ad9b8ff4f172b
Jonathan Pagel <pagel@pobox.com>
2015-03-13 16:06:44 +0000

    new folder for standard express install

---
+++ express/dt_removals/config/db.js
@@ -0,0 +1,22 @@
+var CONFIG = require(process.cwd()+'/config/config').config,
+    Sequelize = require('sequelize');
+console.log(CONFIG);
+
+var sequelize = new Sequelize(CONFIG.db.name, CONFIG.db.user, CONFIG.db.password, {
+        dialect: "mysql",
+        port:    3306,
+    });
+
+sequelize
+    .authenticate()
+    .complete(function(err) {
+        if (!!err) {
+            console.log('Unable to connect to the database:', err)
+        } else {
+            console.log('Connection has been established successfully.')
+        }
+    });
+
+exports.db = {
+    sequelize : sequelize,
+};
