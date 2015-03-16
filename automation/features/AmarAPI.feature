ba2ba565fecacb179e8a2a1359cdebe638e3f14f
amar fiaz <amar_fiaz@hotmail.com>
2015-03-16 14:49:14 +0000

    stuff

--- automation/features/AmarAPI.feature
+++ automation/features/AmarAPI.feature
@@ -3,9 +3,35 @@ Feature: Let's make some calls to AmarAPI & se what happens
  I want to test AmarAPI
  So that it will bring smile for behat community
  
-Scenario: GitHub behat Demo Api
-  Given I request "/notes"
-  Then the response should be JSON
-  And the response status code should be 200
-  And the response has a "title" property
-  And the "tile" property equals "AmarYeah"
+#Scenario: GitHub behat Demo Api
+#  Given I request "/dashboard"
+#  Then the response should be JSON
+#  And the status code should be 200
+#  And the response has a "title" property
+#  And the "title" property equals "AmarYeah"
+
+
+Scenario: updating data by sending JSON
+  Given I post the following JSON to "/update-centres":
+  """
+  {
+    "totals" : {
+        "date" : "27-2-2015",
+        "time" : "08:02:37",
+        "bed_counts": {
+            "Harmondsworth" : {
+                "male" : 20,
+                "female" : 8,
+                "out_of_commission": 1
+            },
+            "Brook House" : {
+                "male" : 5,
+                "female" : 0,
+                "out_of_commission": 0
+            }
+        }
+    }
+}
+"""
+Then the status code should be 200
+
--- automation/features/bootstrap/FeatureContext.php
+++ automation/features/bootstrap/FeatureContext.php
@@ -6,13 +6,19 @@ use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Behat\MinkExtension\Context\MinkContext;
 use GuzzleHttp\Client;    
  
-class FeatureContext extends BehatContext
+#class FeatureContext extends BehatContext
+class FeatureContext extends MinkContext implements KernelAwareInterface 
 {
     public $_response;
     public $_client;
     private $_parameters = array();
+    
     /**
      * Initializes context.
      * Every scenario gets it's own context object.
@@ -40,6 +46,15 @@ class FeatureContext extends BehatContext
     }
  
     /**
+     * @Given /^I post the following JSON to "([^"]*)":$/
+     */
+    public function iPostTheFollowingJsonToTheApi($uri, PyStringNode $JSON)
+    {
+        $request = $this->_client->post($uri, ["body"=> $JSON]);
+        $this->_response = $request;
+    }
+
+    /**
      * @When /^I request "([^"]*)"$/
      */
     public function iRequest($uri)
@@ -58,31 +73,35 @@ class FeatureContext extends BehatContext
        }
     }
  
-    /**
-     * @Then /^the response status code should be (\d+)$/
-     */
-    public function theResponseStatusCodeShouldBe($httpStatus)
-    {
-        if ((string)$this->_response->getStatusCode() !== $httpStatus) {
+	/**
+ 	* @Given /^the status code should be (\d+)$/
+ 	*/
+	public function theStatusCodeShouldBe($httpStatus)
+	{
+        if ((string)$this->_response->getStatusCode() !== $httpStatus) 
+        {
             throw new \Exception('HTTP code does not match '.$httpStatus.
                 ' (actual: '.$this->_response->getStatusCode().')');
-        }
-    } 
- 
+		}
+	}
+
     /**
      * @Given /^the response has a "([^"]*)" property$/
      */
     public function theResponseHasAProperty($propertyName)
     {
         $data = json_decode($this->_response->getBody(true));
-        if (!empty($data)) {
-            if (!isset($data->$propertyName)) {
+        if (!empty($data)) 
+        {
+            if (!isset($data->$propertyName)) 
+            {
                 throw new Exception("Property '".$propertyName."' is not set!\n");
             }
         } else {
             throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
         }
     }
+
      /**
      * @Then /^the "([^"]*)" property equals "([^"]*)"$/
      */
@@ -101,4 +120,59 @@ class FeatureContext extends BehatContext
             throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
         }
     }
+
+	/**
+   	* Sets HttpKernel instance.
+   	* This method will be automatically called by Symfony2Extension ContextInitializer.
+   	*
+   	* @param KernelInterface $kernel
+   	*/
+  	public function setKernel(KernelInterface $kernel) 
+  	{
+    	$this->kernel = $kernel;
+  	}
+
+    /**
+     * @When /^I type in (\d+) into the "([^"]*)" field$/
+     */
+    public function iTypeInIntoTheField($value, $field)
+    {
+        $field = $this->fixStepArgument($field);
+        $value = $this->fixStepArgument($value);
+        $this->getSession()->getPage()->fillField($field, $value);
+    }
+
+    /**
+     * @Then /^I should see (\d+)$/
+     */
+    public function iShouldSee($text)
+    {
+        $this->assertSession()->pageTextContains($this->fixStepArgument($text));
+    }
+
+  	/**
+  	* @When /^I check the "([^"]*)" radio button$/
+  	*/
+  	public function iCheckTheRadioButton($radioLabel)
+  	{
+        $radioButton = $this->getSession()->getPage()->findField($radioLabel);
+        if (null === $radioButton) 
+        {
+        	throw new Exception('Cannot find radio button '.$radioLabel);
+  		}
+    	$value = $radioButton->getAttribute('value');
+    	$this->getSession()->getDriver()->click($radioButton->getXPath());
+	}
+
+  	/**
+  	* @Then /^Radio button with id "([^"]*)" should be checked$/
+  	*/
+  	public function RadioButtonWithIdShouldBeChecked($sId)
+  	{
+    	$elementByCss = $this->getSession()->getPage()->find('css', 'input[type="radio"]:checked#'.$sId);
+    	if (!$elementByCss) 
+    	{
+        	throw new Exception('Radio button with id ' . $sId.' is not checked');
+    	}
+  	}
 }
