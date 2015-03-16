b081f8b85669725a126c0d95730ac081ab45570a
Jonathan Pagel <pagel@pobox.com>
2015-03-16 10:31:19 +0000

    express install with /dashboard

--- express/removals/routes/dashboard.js
+++ express/removals/routes/dashboard.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+router = express.Router(),
+models = require(process.cwd()+'/lib/models'),
+jade = require("jade"),
+data_reader = require(process.cwd()+"/lib/data_reader.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard', list:['b','h','r'] });
+    var DR = new data_reader();
+    DR.get_centres(function(list){
+console.log(list['Campsfield']);
+        res.render('dashboard', { title: 'Dashboard', 'list':list });
+    });
 });
 
 module.exports = router;
