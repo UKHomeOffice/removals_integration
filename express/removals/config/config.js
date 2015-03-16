60333387f9110c549b8dc2cc82ff997d89468fe7
Jonathan Pagel <pagel@pobox.com>
2015-03-16 13:52:26 +0000

    config

---
+++ express/removals/config/config.js
@@ -0,0 +1,39 @@
+var map= {
+        'DB_USER' : 'username',
+        'DB_PASSWORD' : 'password',
+        'DB_HOST' : 'host',
+        'DB_NAME' : 'database',
+    },
+    get_from_env = Object.keys(map),
+    directory_path_components = process.cwd().split('/'),
+    project_path = directory_path_components.slice(0, directory_path_components.length).join('/'),
+    cur_env = process.env.NODE_ENV || "development",
+    config = {},
+    v;
+
+for(v in get_from_env) {
+    if (get_from_env.hasOwnProperty(v)) {
+        var key = get_from_env[v];
+        if (process.env[key]) {
+            console.log("setting " + key + " to " + process.env[key]);
+            config[map[key]] = process.env[key];
+        } else {
+            console.log("No environment variable " + key + ". Using config.json.");
+            console.log("Current env is " + cur_env);
+            config = require(process.cwd()+'/config/config.json')[cur_env];
+            break;
+        }
+    }
+}
+
+CONFIG = {
+    project_path: project_path,
+    db: {
+        name: config.database,
+        user: config.username,
+        password: config.password
+    }
+};
+console.log(CONFIG);
+
+exports.config = CONFIG;
