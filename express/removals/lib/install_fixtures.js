745860afb909fceaa9a441dd8ed8d311df3bf803
Chris Ovenden <cjo20@gs011461.local>
2015-03-17 17:38:01 +0000

    don't aways run install fixtures

--- express/removals/lib/install_fixtures.js
+++ express/removals/lib/install_fixtures.js
@@ -1,15 +1,17 @@
 var models = require('./models').models,
     utils = require('./utils').utils;
 
-function installFixtures () {
+function installFixtures (wipeDb) {
     utils.import_csv(
-        /*CONFIG.project_path + '/fixtures/nationalities.csv',
-         models.Nationality,
-         true,
-         null*/
-        './fixtures/centres.csv',
-        models.Centre,
-        true,
+       './fixtures/centres.csv',
+       models.Centre,
+       wipeDb,
+       null
+    );
+    utils.import_csv(
+        './fixtures/nationalities.csv',
+        models.Nationality,
+        wipeDb,
         null
     );
 }
