49b77ee025f0a0b6e42ebfd7437db5da405f7b61
Jonathan Pagel <pagel@pobox.com>
2015-03-18 14:00:28 +0000

    adapted tests

--- express/removals/tests/http_tests.js
+++ express/removals/tests/http_tests.js
@@ -1,17 +1,16 @@
 var hippie = require('hippie');
 
 var valid_json = {"totals":{"bed_counts":{"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}};
-var valid_json3 = '{"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield" : { "male" : 20, "female" : 8, "out_of_commission": 1 }, "Harmondsworth" : { "male" : 5, "female" : 0, "out_of_commission": 0 } } } }';
-var valid_json_invalid_centre = '{"totals":{"bed_counts":{"Cindy Opera House":{"male":22,"female":9,"out_of_commission":2},"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}}';
-var valid_json_invalid_centre2 = '{"totals":{"bed_counts":{"Yarls Wood":{"male":22,"female":9,"out_of_commission":2},"Mortuus Plango":{"male":42,"female":19,"out_of_commission":5}}}}';
-var valid_json_invalid_centre3 = '{"totals":{"bed_counts":{"Madstop Bungalo":{"male":22,"female":9,"out_of_commission":2},"Potsdam Cottage":{"male":42,"female":19,"out_of_commission":5}}}}';
+var valid_json3 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield" : { "male" : 20, "female" : 8, "out_of_commission": 1 }, "Harmondsworth" : { "male" : 5, "female" : 0, "out_of_commission": 0 } } } };
+var valid_json_invalid_centre = {"totals":{"bed_counts":{"Cindy Opera House":{"male":22,"female":9,"out_of_commission":2},"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}};
+var valid_json_invalid_centre2 = {"totals":{"bed_counts":{"Yarls Wood":{"male":22,"female":9,"out_of_commission":2},"Mortuus Plango":{"male":42,"female":19,"out_of_commission":5}}}};
+var valid_json_invalid_centre3 = {"totals":{"bed_counts":{"Madstop Bungalo":{"male":22,"female":9,"out_of_commission":2},"Potsdam Cottage":{"male":42,"female":19,"out_of_commission":5}}}};
 var invalid_json = '{"totals":{"bed_counts:{"Yarls Wood":{"male":42,"female":19,"out_of_commission":5}}}}';
 
 function api() {
   return hippie()
     .base('http://localhost:3000')
     .header("User-Agent", "hippie")
-    .json()
     .post('/update-centres');
 }
 /*
@@ -24,13 +23,13 @@ api()
 */
 
 api()
-    .send(valid_json)
+    .json()
+    .send(valid_json3)
     .expectStatus(200)
     .end(function(err,res,body) {
         console.log(body);
         if(err) throw err;
     });
-/*
 api()
     .send(invalid_json)
     .expectStatus(400)
@@ -40,6 +39,7 @@ api()
     });
 
 api()
+    .json()
     .send(valid_json_invalid_centre)
     .expectStatus(404)
     .end(function(err,res,body) {
@@ -48,6 +48,7 @@ api()
     });
 
 api()
+    .json()
     .send(valid_json_invalid_centre2)
     .expectStatus(404)
     .end(function(err,res,body) {
@@ -56,10 +57,10 @@ api()
     });
 
 api()
+    .json()
     .send(valid_json_invalid_centre3)
     .expectStatus(404)
     .end(function(err,res,body) {
         console.log(body);
         if(err) throw err;
     });
-*/
