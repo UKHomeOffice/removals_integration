1132fee6ed7e85fd70eddecbdcb3731cbf5ac625
Jonathan Pagel <pagel@pobox.com>
2015-03-20 11:14:31 +0000

    sniffing for empty input. post must have content-type set to application/json

--- express/removals/routes/update_centres.js
+++ express/removals/routes/update_centres.js
@@ -6,29 +6,33 @@ var express = require('express'),
 router.post('/', function(req, res, next) {
     req.setEncoding("utf8");
     var first_key = Object.keys(req.body)[0];
-    if('{' == first_key.substr(0,1)) {
-        var postData = first_key;
+    if('undefined' == typeof(first_key)){
+        res.status(400).json({"status":"ERROR","error":"empty body"});
     } else {
-        var postData = req.body;
-    }
-    if('string' == typeof(postData)){
-        postData = JSON.parse(postData);
-    }
-    JW = new json_wrangler(true);
-    try{
-        JW.consume(postData,function(success, error){})
-            .then(function(obj){
-                res.status(200).json({"status":"OK"});
-                io.emit("centre-update",postData);
-            })
-            .then(function(){
-                JW.update_centres();
-            })
-            .then(null,function(err){
-                res.status(404).json({"status":"ERROR","error":err});
-        });
-    }catch(err){
-        res.status(400).json({"status":"ERROR","error":err});
+        if('{' == first_key.substr(0,1)) {
+            var postData = first_key;
+        } else {
+            var postData = req.body;
+        }
+        if('string' == typeof(postData)){
+            postData = JSON.parse(postData);
+        }
+        JW = new json_wrangler(true);
+        try{
+            JW.consume(postData,function(success, error){})
+                .then(function(obj){
+                    res.status(200).json({"status":"OK"});
+                    io.emit("centre-update",postData);
+                })
+                .then(function(){
+                    JW.update_centres();
+                })
+                .then(null,function(err){
+                    res.status(404).json({"status":"ERROR","error":err});
+            });
+        }catch(err){
+            res.status(400).json({"status":"ERROR","error":err});
+        }
     }
 });
 
