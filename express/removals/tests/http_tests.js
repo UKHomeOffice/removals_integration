8811719cd9a3f4b42a59c76adef81e0e19c605a0
Jonathan Pagel <pagel@pobox.com>
2015-03-23 15:58:10 +0000

    jquery transition when centre updated

--- express/removals/tests/http_tests.js
+++ express/removals/tests/http_tests.js
@@ -1,7 +1,7 @@
 var hippie = require('hippie');
 
 var valid_json = {"totals":{"bed_counts":{"Colnbrook":{"male":1,"female":2,"out_of_commission":4}}}};
-var valid_json3 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield":{"male":51,"female":2,"out_of_commission":3} ,"Colnbrook" : { "male" : 21, "female" : 8, "out_of_commission": 1 }, "Harmondsworth" : { "male" : 17, "out_of_commission": 1 } } } };
+var valid_json3 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield":{"male":51,"female":2,"out_of_commission":3} ,"Harmondsworth" : { "male" : 17, "out_of_commission": 1 } } } };
 var valid_json_invalid_centre = {"totals":{"bed_counts":{"Cindy Opera House":{"male":22,"female":9,"out_of_commission":2},"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}};
 var valid_json_invalid_centre2 = {"totals":{"bed_counts":{"Yarls Wood":{"male":22,"female":9,"out_of_commission":2},"Mortuus Plango":{"male":42,"female":19,"out_of_commission":5}}}};
 var valid_json_invalid_centre3 = {"totals":{"bed_counts":{"Madstop Bungalo":{"male":22,"female":9,"out_of_commission":2},"Potsdam Cottage":{"male":42,"female":19,"out_of_commission":5}}}};
