c0aa140ba76afb2c036ff047f10b1f47dea8abee
Chris Ovenden <cjo20@gs011461.local>
2015-03-25 17:41:17 +0000

    data that makes sense

--- express/removals/tests/http_tests2.js
+++ express/removals/tests/http_tests2.js
@@ -1,7 +1,7 @@
 var hippie = require('hippie');
 
 var valid_json = '{"totals":{"bed_counts":{"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}}';
-var valid_json3 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield" : { "male" : 0, "female" : 1, "out_of_commission": 5 }, "Harmondsworth" : { "male" : 25, "female" : 1, "out_of_commission": 2 } } } };
+var valid_json3 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Colnbrook" : { "male" : 0, "female" : 1, "out_of_commission": 5 }, "Harmondsworth" : { "male" : 0, "out_of_commission": 2 } } } };
 var valid_json_invalid_centre = '{"totals":{"bed_counts":{"Cindy Opera House":{"male":22,"female":9,"out_of_commission":2},"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}}';
 var valid_json_invalid_centre2 = '{"totals":{"bed_counts":{"Yarls Wood":{"male":22,"female":9,"out_of_commission":2},"Mortuus Plango":{"male":42,"female":19,"out_of_commission":5}}}}';
 var valid_json_invalid_centre3 = '{"totals":{"bed_counts":{"Madstop Bungalo":{"male":22,"female":9,"out_of_commission":2},"Potsdam Cottage":{"male":42,"female":19,"out_of_commission":5}}}}';
