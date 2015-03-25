1b8cbe1beed8f8e5a9ab1299f980f19e7b8c989a
Chris Ovenden <cjo20@gs011461.local>
2015-03-25 13:28:40 +0000

    lastupdated now updates correctly

--- express/removals/public/javascripts/updates.js
+++ express/removals/public/javascripts/updates.js
@@ -4,15 +4,18 @@ $(function() {
     function dashboard_update_centre(name, bed_counts) {
         var slug = name.replace(/([^\w])/g,'').toLowerCase(),
             $panel = $("#" + slug),
-            $ele = $panel.find('ul.availability'),
-            centre_panel_html = template_centre_panel({centre:bed_counts}),
-            $replacement = $(centre_panel_html).filter('ul.availability');
+            $ul = $panel.find('ul.availability'),
+            $tailer = $panel.find('.tailer'),
+            $centre_panel_html = $(template_centre_panel({centre:bed_counts})),
+            $replacement = $centre_panel_html.filter('ul.availability'),
+            $lastUpdated = $centre_panel_html.filter('.tailer');
         bed_counts.slug = slug;
         bed_counts.updatedAt = null;
 
-        $ele.replaceWith($replacement);
+        $ul.replaceWith($replacement);
+        $tailer.replaceWith($lastUpdated);
 
-        if (0 === bed_counts.male || 0 === bed_counts.female) {
+        if (0 === bed_counts.male) {
             $panel.addClass('full');
         } else {
             $panel.removeClass('full');
