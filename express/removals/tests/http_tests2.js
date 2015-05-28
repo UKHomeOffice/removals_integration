93bef64f8f901def7c00eaf6e02cc3752824f6b2
Jonathan Pagel <pagel@pobox.com>
2015-05-28 10:46:57 +0100

    making the test jsons somewhat clearer

--- express/removals/tests/http_tests2.js
+++ express/removals/tests/http_tests2.js
@@ -1,11 +1,14 @@
 var hippie = require('hippie');
 
+/*
 var valid_json = {"totals":{"bed_counts":{"Colnbrook":{"male":1,"female":2,"out_of_commission":4}}}};
-var valid_json4 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield":{"male":51,"out_of_commission":3}, "Colnbrook" : {"male":211,"female":0,"out_of_commission":10  } ,"Harmondsworth" : { "male" : 17, "out_of_commission": 1 } } } };
 var valid_json_invalid_centre = {"totals":{"bed_counts":{"Cindy Opera House":{"male":22,"female":9,"out_of_commission":2},"Morton Hall":{"male":42,"female":19,"out_of_commission":5}}}};
 var valid_json_invalid_centre2 = {"totals":{"bed_counts":{"Yarls Wood":{"male":22,"female":9,"out_of_commission":2},"Mortuus Plango":{"male":42,"female":19,"out_of_commission":5}}}};
 var valid_json_invalid_centre3 = {"totals":{"bed_counts":{"Madstop Bungalo":{"male":22,"female":9,"out_of_commission":2},"Potsdam Cottage":{"male":42,"female":19,"out_of_commission":5}}}};
 var invalid_json = '{"totals":{"bed_counts:{"Yarls Wood":{"male":42,"female":19,"out_of_commission":5}}}}';
+*/
+
+var valid_json4 = {"totals" : { "date" : "27-2-2015", "time" : "08:02:37", "bed_counts": { "Campsfield":{"male":51,"out_of_commission":3}, "Colnbrook" : {"male":211,"female":0,"out_of_commission":10  } ,"Harmondsworth" : { "male" : 17, "out_of_commission": 1 } } } };
 
 function api() {
   return hippie()
