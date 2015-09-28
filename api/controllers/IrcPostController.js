4c2982548b45185947984dd2074c78e725f95269
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-09-28 13:56:53 +0100

    initial json validation occuring

---
+++ api/controllers/IrcPostController.js
@@ -0,0 +1,26 @@
+/**
+ * IrcPostController
+ *
+ * @description :: Server-side logic for managing ircposts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  /**
+   * `IrcPostController.post()`
+   */
+  post: function (req, res) {
+    var validate = require('jsonschema').validate;
+
+    var validation_schema = require('./request_schemas/irc.json');
+
+    var validation_response = validate(req.body, validation_schema);
+
+    if (validation_response.errors.length > 0) {
+      return res.status(400).json(validation_response.errors);
+    }
+    return res.status(200).send('OK');
+  }
+};
+
---
+++ test/unit/controllers/IrcPostController.test.js
@@ -0,0 +1,33 @@
+var request = require('supertest');
+var _ = require('lodash');
+
+describe('IrcPostController', function () {
+
+  describe('JS Validation', function () {
+    describe('all scenarios should pass json schema check', function () {
+      var scenarios = [
+        {bed_in_of_commission: require('../../scenarios/bed_in_of_commission.json')},
+        {bed_out_of_commission: require('../../scenarios/bed_out_of_commission.json')},
+        {detainee_checked_in: require('../../scenarios/detainee_checked_in.json')},
+        {detainee_checked_out: require('../../scenarios/detainee_checked_out.json')},
+        {inter_centre_move: require('../../scenarios/inter_centre_move.json')},
+      ];
+      return _.map(scenarios, function (scenario) {
+        return it(Object.keys(scenario)[0] + ' should pass json schema check', function () {
+          return request(sails.hooks.http.app)
+            .post('/IrcPost/post')
+            .send(scenario[Object.keys(scenario)[0]])
+            .expect(200);
+
+        });
+      });
+    });
+
+    it('should fail invalid json', function () {
+      return request(sails.hooks.http.app)
+        .post('/IrcPost/post')
+        .send('4')
+        .expect(400);
+    });
+  });
+});
