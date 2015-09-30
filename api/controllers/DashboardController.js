180ce2b651632ec3a1f85f246281cd105f7c1a9c
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-09-30 09:37:17 +0100

    output dashboard json to current draft contract

--- api/controllers/DashboardController.js
+++ api/controllers/DashboardController.js
@@ -5,8 +5,6 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-var Promise;
-Promise = require('bluebird');
 module.exports = {
 
   /**
@@ -15,6 +13,18 @@ module.exports = {
   index: function (req, res) {
     return Centre.find()
       .catch(res.serverError)
+      .map(function (centre) {
+        return {
+          name: centre.name,
+          centre_id: centre.id,
+          male_capacity: centre.male_capacity,
+          male_available: centre.male_capacity - centre.male_in_use - centre.male_out_of_commission,
+          female_capacity: centre.female_capacity,
+          female_available: centre.female_capacity - centre.female_in_use - centre.female_out_of_commission,
+          booked: 0,
+          reserved: 0
+        };
+      })
       .then(res.ok);
   },
 };
--- test/unit/controllers/DashboardController.test.js
+++ test/unit/controllers/DashboardController.test.js
@@ -1,15 +1,24 @@
 var request = require('supertest');
-var _ = require('lodash');
 var chai = require('chai');
 var expect = chai.expect;
 
 describe('DashboardController', function () {
-  it('should fail invalid json', function () {
+  it('should return a valid json dashboard', function () {
     return request(sails.hooks.http.app)
       .get('/Dashboard')
       .expect(200)
       .expect(function (res) {
-        expect(res.body).to.have.length(3);
+        return expect(res.body).to.have.length(3).and.to.contain(
+          {
+            name: 'bigone',
+            centre_id: 1,
+            male_capacity: 1000,
+            male_available: 1000,
+            female_capacity: 2500,
+            female_available: 2500,
+            booked: 0,
+            reserved: 0
+          });
       });
   });
 });
