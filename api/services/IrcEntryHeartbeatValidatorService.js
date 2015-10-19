3085f78a53a71c7f17bbd914c92b37723cc78b64
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-10-19 15:21:35 +0100

    export the schema from the validator services

--- api/services/IrcEntryHeartbeatValidatorService.js
+++ api/services/IrcEntryHeartbeatValidatorService.js
@@ -3,5 +3,6 @@ var validation_schema = require('removals_schema').heartbeat;
 module.exports = {
   validate: function (request_body) {
     return RequestValidatorService.validate(request_body, validation_schema);
-  }
+  },
+  schema: validation_schema
 };
