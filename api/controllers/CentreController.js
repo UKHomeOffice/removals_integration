53b4adbf42cc07019494cbfff1eb2e998a01c184
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-10-20 14:33:15 +0100

    added centre controller with CRUD tests

---
+++ api/controllers/CentreController.js
@@ -0,0 +1,9 @@
+/**
+ * CentreController
+ *
+ * @description :: Server-side logic for managing centres
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
+
---
+++ test/unit/controllers/CentreController.test.js
@@ -0,0 +1,53 @@
+var jhg = require('../../helpers/JsonHelperGenerator');
+
+describe('CentreController', function () {
+  var controller;
+  before(function () {
+    controller = sails.controllers.centre;
+  });
+
+  it('should be able to get a list of all the centres', function () {
+    return request(sails.hooks.http.app)
+      .get('/centre')
+      .expect(200)
+      .expect(function (res) {
+        return expect(res.body).to.have.length(3)
+          .and.to.contain.a.thing.with.property('id', 1)
+          .and.to.contain.a.thing.with.property('name', 'bigone');
+      });
+  });
+
+  it('should be able to add a new centre', function () {
+    return request(sails.hooks.http.app)
+      .post('/centre')
+      .send()
+      .expect(201)
+      .expect(function (res) {
+        return expect(res.body).to.have.property('id');
+      });
+  });
+
+  it('should be able to update an existing centre', function () {
+    return request(sails.hooks.http.app)
+      .post('/centre/1')
+      .send({name: "renamed"})
+      .expect(200)
+      .then(function () {
+        return Centre.findOne(1).then(function (centre) {
+          return expect(centre).to.have.property('name', 'renamed');
+        });
+      });
+  });
+
+  it('should be able to delete an existing centre', function () {
+    return request(sails.hooks.http.app)
+      .delete('/centre/1')
+      .expect(200)
+      .then(function () {
+        return Centre.findOne(1).then(function (centre) {
+          return expect(centre).to.be.empty;
+        });
+      });
+  });
+
+});
