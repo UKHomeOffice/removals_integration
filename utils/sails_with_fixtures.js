be4c8cdf66b21c3d0c2b04ca76e36948292a2e7d
ashley-hoit <ashleywatson.uk@gmail.com>
2015-10-28 11:48:21 +0000

    start up tests with fixtures in

---
+++ utils/sails_with_fixtures.js
@@ -0,0 +1,21 @@
+var Sails = require('sails');
+var Barrels = require('barrels');
+var Promise = require('bluebird');
+var request = require('supertest-as-promised');
+var _ = require('lodash');
+
+Sails.lift(
+  {
+    models: {
+      connection: 'test',
+      migrate: 'drop'
+    }
+  },
+  function (err, sails) {
+    var barrels = new Barrels();
+    barrels.populate(function (err) {
+      sails.log('up');
+    });
+  }
+)
+;
