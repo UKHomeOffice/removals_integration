d72084ecdf55fbcabf89855688274095b6689acb
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-11-04 09:41:52 +0000

    add list of users to development page

--- api/controllers/DevelopmentController.js
+++ api/controllers/DevelopmentController.js
@@ -16,10 +16,12 @@ module.exports = {
     response.networkInterfaces = os.networkInterfaces();
     response.freemem = os.freemem();
     var format = '{%n  "commit": "%H",%n  "abbreviated_commit": "%h",%n  "tree": "%T",%n  "abbreviated_tree": "%t",%n  "parent": "%P",%n  "abbreviated_parent": "%p",%n  "refs": "%D",%n  "encoding": "%e",%n  "subject": "%s",%n  "sanitized_subject_line": "%f",%n  "body": "%b",%n  "commit_notes": "%N",%n  "verification_flag": "%G?",%n  "signer": "%GS",%n  "signer_key": "%GK",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  },%n  "commiter": {%n    "name": "%cN",%n    "email": "%cE",%n    "date": "%cD"%n  }%n},';
-
-    child_process.exec('git log -n 20  --pretty=format:\'' + format + '\'', function (error, stdout, stderr) {
-      response.git = JSON.parse('[' + stdout.slice(0, -1) + ']');
-      res.ok(response);
+    User.find({}).then(users => {
+      response.users = users;
+      child_process.exec('git log -n 20  --pretty=format:\'' + format + '\'', function (error, stdout, stderr) {
+        response.git = JSON.parse('[' + stdout.slice(0, -1) + ']');
+        res.ok(response);
+      });
     });
   }
 }
