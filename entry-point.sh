bd812af3d3b198a408b414d60945e3f43f59837c
Allan Degnan <allan@adegnan.net>
2015-12-08 12:09:48 +0000

    added bash to entrypoint to pickup secrets if avail

--- entry-point.sh
+++ entry-point.sh
@@ -7,4 +7,11 @@ else
     CMD=( "$@" )
 fi
 
+#Pickup any secrets
+for f in /etc/secrets/* ; do 
+    if test -f "$f"; then 
+        export "$(basename $f)=$(cat $f)"
+    fi
+done
+
 exec ${CMD[*]}
