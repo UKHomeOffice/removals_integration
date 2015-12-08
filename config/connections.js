347c89ded56d685d3dc057907629a1bc4694573b
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-12-08 09:50:24 +0000

    added SSL env var

--- config/connections.js
+++ config/connections.js
@@ -58,7 +58,8 @@ module.exports.connections = {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: process.env.DB_SSL
   },
   /***************************************************************************
    *                                                                          *
