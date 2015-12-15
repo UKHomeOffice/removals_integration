965f2f46bd565e29a4de923c5b87c4d040de0d29
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-12-15 14:08:17 +0000

    remove references to users

--- api/services/PassportService.js
+++
@@ -1,7 +0,0 @@
-var passport = require('passport');
-var POISEStrategy = require('passportjs-header').Strategy;
-
-module.exports = passport.use(new POISEStrategy(
-  (userid, done) => User.findOne(userid, done)
-));
-
