71bd348dde0b703163434ae2e6fc29268d461f03
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-01-15 13:29:49 +0000

    initial pass at a dashboard controller

---
+++ api/controllers/DashboardController.js
@@ -0,0 +1,53 @@
+module.exports = {
+  _config: {
+    actions: false,
+    shortcuts: false,
+    rest: false,
+    exposedMethods: [
+      'index'
+    ]
+  },
+
+  roomName: 'dashboard',
+
+  index: function (req, res) {
+    Centre.find()
+      .then(centres => centres)
+      .map(this.populateCentre)
+      .then(res.ok)
+      .tap(() => this.joinDashboardRoom(req, res))
+  },
+
+  joinDashboardRoom: function (req, res) {
+    if (_.isObject(req.socket)) {
+      sails.sockets.join(req.socket, this.roomName)
+    }
+  },
+
+  broadcastAllCentreUpdates: function () {
+    Centre.find()
+      .then(centres => centres)
+      .map(this.broadcastCentreUpdate)
+  },
+
+  broadcastCentreUpdate: function (centre) {
+    this.populateCentre(centre)
+      .then(centre => sails.sockets.broadcast(this.roomName, centre))
+  },
+
+  countMovements: movements => _.countBy(movements, movement => movement.detainee.gender),
+
+  populateCentre: function (centre) {
+    return Movement.find({
+        active: true,
+        centre: centre.id
+      })
+      .populate('detainee')
+      .then(this.countMovements)
+      .then(countedMovements => {
+        centre.male_movements = countedMovements.male || 0;
+        centre.female_movements = countedMovements.female || 0;
+        return centre;
+      })
+  },
+};
