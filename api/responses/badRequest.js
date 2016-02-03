035e55a5c179fe7a28dca53435324e1e27ffbe61
JoeChapman <joseph.philip.chapman@gmail.com>
2016-02-03 11:10:15 +0000

    BM-403: Remove non-json responses
    - make all reponses return json
    - remove error response views

--- api/responses/badRequest.js
+++ api/responses/badRequest.js
@@ -1,30 +1,11 @@
 'use strict';
 
-/**
- * 400 (Bad Request) Handler
- *
- * Usage:
- * return res.badRequest();
- * return res.badRequest(data);
- * return res.badRequest(data, 'some/specific/badRequest/view');
- *
- * e.g.:
- * ```
- * return res.badRequest(
- *   'Please choose a valid `password` (6-12 characters)',
- *   'trial/signup'
- * );
- * ```
- */
-
 const badRequestCode = 400;
 
-module.exports = function badRequest (data, options) {
+module.exports = function badRequest (data) {
   var req = this.req;
   var res = this.res;
   var sails = req._sails;
-
-  options = options || {};
 
   res.status(badRequestCode);
 
@@ -34,32 +15,10 @@ module.exports = function badRequest (data, options) {
     sails.log.verbose('Sending 400 ("Bad Request") response: \n', data);
   }
 
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
   if (sails.config.environment === 'production') {
     data = undefined;
   }
 
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  if (typeof options === 'string') {
-    options = {view: options};
-  }
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, {data: data});
-  }
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  return res.guessView({data: data}, () => res.jsonx(data));
+  return res.jsonx(data);
 };
 
--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -1,27 +1,11 @@
 'use strict';
 
-/**
- * 500 (Server Error) Response
- *
- * Usage:
- * return res.serverError();
- * return res.serverError(err);
- * return res.serverError(err, 'some/specific/error/view');
- *
- * NOTE:
- * If something throws in a policy or controller, or an internal
- * error is encountered, Sails will call `res.serverError()`
- * automatically.
- */
-
 const serverErrorCode = 500;
 
-module.exports = function serverError (data, options) {
+module.exports = function serverError (data) {
   var req = this.req;
   var res = this.res;
   var sails = req._sails;
-
-  options = options || {};
 
   res.status(serverErrorCode);
 
@@ -31,43 +15,9 @@ module.exports = function serverError (data, options) {
     sails.log.error('Sending 500 ("Server Error") response: \n', data);
   }
 
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
   if (sails.config.environment === 'production') {
     data = undefined;
   }
 
-  if (req.wantsJSON) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  if (typeof options === 'string') {
-    options = {view: options};
-  }
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-  // If no second argument provided, try to serve the default view,
-  // but fall back to sending JSON(P) if any errors occur.
-  return res.view('500', { data: data }, function (err, html) {
-    // If a view error occured, fall back to JSON(P).
-    if (err) {
-      // If the view was missing, ignore the error but provide a verbose log.
-      if (err.code === 'E_VIEW_FAILED') {
-        sails.log.verbose('res.serverError() :: Could not locate view for error page (sending JSON instead).  Details: ', err);
-      } else {
-        // Otherwise, if this was a more serious error, log to the console with the details.
-        sails.log.warn('res.serverError() :: When attempting to render error page view, an error occured (sending JSON instead). Details: ', err);
-      }
-      return res.jsonx(data);
-    }
-    return res.send(html);
-  });
+  return res.jsonx(data);
 };
