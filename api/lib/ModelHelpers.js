a7d6ebe43f5748fc43f05b69c2e72642bf13fb83
cksanders <cksanders1@gmail.com>
2016-04-08 13:09:12 +0100

    IBM-30: Updated ModelHelpers to set Moment Time Zone

--- api/lib/ModelHelpers.js
+++ api/lib/ModelHelpers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  findAndUpdateOrCreate: function findAndUpdateOrCreate (criteria, values) {
+  findAndUpdateOrCreate: function findAndUpdateOrCreate(criteria, values) {
     return this.update(criteria, values)
       .then(result => _.isEmpty(result) ? this.create(values) : result[0]);
   },
