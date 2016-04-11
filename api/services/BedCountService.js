de0b30508d66a18d97c2e7e5eb023d685b0bd2ea
Robin Knipe <robin.knipe@gmail.com>
2016-04-11 10:52:49 +0100

    Minior refactor for code clarity, and to remove unnecessary functionality. Also ensured the data is correctly sorted to optimise reconciliation checks.

--- api/services/BedCountService.js
+++ api/services/BedCountService.js
@@ -10,39 +10,29 @@ function DateRange(from, to) {
   this.to = to;
 }
 
-DateRange.widen = function () {
-  var args = Array.prototype.slice.call(arguments);
-  var from = args.map((range) => range.from);
-  var resultingFrom = from.reduce((a, b) => a < b ? a : b);
-
-  var to = args.map((range) => range.to);
-  var resultingTo = to.reduce((a, b) => a > b ? a : b);
-
-  return new DateRange(resultingFrom, resultingTo);
-};
-
 const fullRange = (visibilityRange, rangeFactory) =>
-  DateRange.widen.apply([visibilityRange], [visibilityRange.from, visibilityRange.to].map((date) => rangeFactory(date)));
+  new DateRange(rangeFactory(visibilityRange.from).from, rangeFactory(visibilityRange.to).to);
 
 const populate = (barrel, centreId, range) =>
   barrel.find({
-    centre: centreId,
-    timestamp: {
-      '>=': range.from,
-      '<=': range.to
-    }
+    where: {
+      centre: centreId,
+        timestamp: { '>=': range.from, '<=': range.to }
+    },
+    sort: 'timestamp'
   });
 const populateEvents = (centre, range) =>
   populate(Event, centre.id, range)
     .populate('detainee')
     .then(events => centre.unreconciledEvents = events);
-const populateMovements = (centre, range) =>
+const populateMovements = (centre, range) => () =>
   populate(Movement, centre.id, range)
     .then(movements => centre.unreconciledMovements = movements);
 
-const reconcileEvents = (centre, reconciler) => {
+const reconcileEvents = (centre, reconciler) => () => {
   centre.unreconciledEvents = centre.unreconciledEvents.filter((event) =>
-    !centre.unreconciledMovements.some((movement, movementKey) => reconcile(movement, movementKey, centre, event, reconciler))
+    !centre.unreconciledMovements.some((movement, movementKey) =>
+      reconcile(movement, movementKey, centre, event, reconciler))
   );
   centre.unreconciledMovements = centre.unreconciledMovements.filter((movement) => movement !== null);
 };
@@ -58,14 +48,10 @@ const reconciliationTester = (movementsRangeFactory, eventsRangeFactory) => (eve
   movement.cid_id === event.detainee.cid_id && (movementsRangeFactory(event.timestamp).contains(movement.timestamp)
     || eventsRangeFactory(movement.timestamp).contains(event.timestamp));
 
-const filterUnreconciled = (centre, range) => {
+const filterUnreconciled = (centre, range) => () => {
   centre.unreconciledEvents = centre.unreconciledEvents.filter(event => range.contains(event.timestamp));
   centre.unreconciledMovements = centre.unreconciledMovements.filter(movement => range.contains(movement.timestamp));
 };
-// const filterReconciled = (centre, range) => // FIXME: pretty sure we don't need to do this!!!
-//   centre.reconciled = centre.reconciled.filter(reconciled =>
-//     range.contains(reconciled.event.timestamp) || range.contains(reconciled.movement.timestamp)
-//   );
 
 module.exports = {
   DateRange,
@@ -79,10 +65,10 @@ module.exports = {
     centre.unreconciledMovements = [];
     centre.reconciled = [];
 
-    return populateEvents(centre, rangeOfEvents).tap(() => populateMovements(centre, rangeOfMovements))
-      .tap(() => reconcileEvents(centre, reconciler))
-      .tap(() => filterUnreconciled(centre, visibilityRange))
-      // .tap(() => filterReconciled(centre, visibilityRange)) // FIXME: pretty sure we don't need to do this!!!
+    return populateEvents(centre, rangeOfEvents)
+      .tap(populateMovements(centre, rangeOfMovements))
+      .tap(reconcileEvents(centre, reconciler))
+      .tap(filterUnreconciled(centre, visibilityRange))
       .return(centre);
   }
 };
