21750950e931edcef593a3f5cc85f31b5090cd55
cksanders <cksanders1@gmail.com>
2016-06-08 12:49:56 +0100

    IBM:86 - Bed Model

---
+++ test/unit/models/Bed.test.js
@@ -0,0 +1,11 @@
+"use strict";
+var model = rewire('../../api/models/Bed');
+
+describe('UNIT BedModel', () => {
+});
+
+describe('INTEGRATION BedModel', () => {
+  it('should get the fixtures', () =>
+    expect(Bed.find()).to.eventually.have.length(15)
+  );
+});
