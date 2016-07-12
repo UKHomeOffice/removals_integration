f80a4a8a6048666e12123705efcbed83cf0f5348
cksanders <cksanders1@gmail.com>
2016-07-12 17:03:48 +0100

    IBM-271: Blacklist nginxId from sails query

--- api/controllers/RootController.js
+++ api/controllers/RootController.js
@@ -1,9 +1,11 @@
 'use strict';
+var _ = require('lodash');
 
 module.exports = {
   index: (req, res) => {
     /* eslint no-param-reassign:0 */
     res.customfields = {id: 'links.centres', value: '/centres', transforms: ['baseurl']};
+    _.set(req, 'options.criteria.blacklist', ['limit', 'skip', 'sort', 'populate', 'nginxId']);
     /* eslint no-param-reassign:2 */
     return res.ok();
   }
