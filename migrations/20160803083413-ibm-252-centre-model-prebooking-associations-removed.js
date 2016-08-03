dc667eb48c17b0b57d7b85ee4cc5770236e74d7f
cksanders <cksanders1@gmail.com>
2016-08-03 09:35:52 +0100

    IBM-252: Centre Model Migrations to remove Prebooking associations

---
+++ migrations/20160803083413-ibm-252-centre-model-prebooking-associations-removed.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const cps = require('cps');
+
+exports.up = (db, callback) =>
+  cps.seq([
+    (_, callback) => db.removeColumn('centre', 'male_prebooking', callback),
+    (_, callback) => db.removeColumn('centre', 'female_prebooking', callback),
+    (_, callback) => db.removeColumn('centre', 'male_contingency', callback),
+    (_, callback) => db.removeColumn('centre', 'female_contingency', callback)
+  ], callback);
+
+exports.down = (db, callback) =>
+  cps.seq([
+    (_, callback) => db.addColumn('centre', 'male_prebooking', 'int', callback),
+    (_, callback) => db.addColumn('centre', 'female_prebooking', 'int', callback),
+    (_, callback) => db.addColumn('centre', 'male_contingency', 'int', callback),
+    (_, callback) => db.addColumn('centre', 'female_contingency', 'int', callback),
+  ], callback);
