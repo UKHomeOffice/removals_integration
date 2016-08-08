7ee42d817399615c9ecf9eb30b4172cc66dca848
cksanders <cksanders1@gmail.com>
2016-08-08 15:27:09 +0100

    this

--- migrations/20160803083413-ibm-252-centre-model-prebooking-associations-removed.js
+++ migrations/20160803083413-ibm-252-centre-model-prebooking-associations-removed.js
@@ -4,16 +4,16 @@ const cps = require('cps');
 
 exports.up = (db, callback) =>
   cps.seq([
-    (_, callback) => db.removeColumn('centre', 'male_prebooking', callback),
-    (_, callback) => db.removeColumn('centre', 'female_prebooking', callback),
-    (_, callback) => db.removeColumn('centre', 'male_contingency', callback),
-    (_, callback) => db.removeColumn('centre', 'female_contingency', callback)
+    (_, callback) => db.removeColumn('centres', 'male_prebooking', callback),
+    (_, callback) => db.removeColumn('centres', 'female_prebooking', callback),
+    (_, callback) => db.removeColumn('centres', 'male_contingency', callback),
+    (_, callback) => db.removeColumn('centres', 'female_contingency', callback)
   ], callback);
 
 exports.down = (db, callback) =>
   cps.seq([
-    (_, callback) => db.addColumn('centre', 'male_prebooking', 'int', callback),
-    (_, callback) => db.addColumn('centre', 'female_prebooking', 'int', callback),
-    (_, callback) => db.addColumn('centre', 'male_contingency', 'int', callback),
-    (_, callback) => db.addColumn('centre', 'female_contingency', 'int', callback)
+    (_, callback) => db.addColumn('centres', 'male_prebooking', 'int', callback),
+    (_, callback) => db.addColumn('centres', 'female_prebooking', 'int', callback),
+    (_, callback) => db.addColumn('centres', 'male_contingency', 'int', callback),
+    (_, callback) => db.addColumn('centres', 'female_contingency', 'int', callback)
   ], callback);
