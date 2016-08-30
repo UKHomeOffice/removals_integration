3df65707cce14838cbb0093b88cdbcf0891bf9f8
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-08-30 15:51:55 +0100

    add links to other controllers from root controller

--- api/controllers/RootController.js
+++ api/controllers/RootController.js
@@ -3,7 +3,48 @@
 module.exports = {
   index: (req, res) => {
     /* eslint no-param-reassign:0 */
-    res.customfields = {id: 'links.centres', value: '/centres', transforms: ['baseurl']};
+    res.customfields = [
+      {
+        id: 'links.centres',
+        value: '/centres',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.ports',
+        value: '/port',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.health',
+        value: '/health',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.heartbeat',
+        value: '/heartbeat',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.bedevent',
+        value: '/bedevent',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.cid_entry',
+        value: '/cid_entry',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.irc_entry',
+        value: '/irc_entry',
+        transforms: ['baseurl']
+      },
+      {
+        id: 'links.depmu_entry',
+        value: '/depmu_entry',
+        transforms: ['baseurl']
+      }
+    ];
     /* eslint no-param-reassign:2 */
     return res.ok();
   }
